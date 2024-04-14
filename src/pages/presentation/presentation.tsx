781fedb8c782fa4a5943402b4bb83f551076e643
Lucas Campanelli <lucas.campanelli@outlook.com>
2024-04-14 04:33:04 -0300

    Configurando o router

---
+++ src/pages/presentation/presentation.tsx
@@ -0,0 +1,8 @@
+export default function Presentation() {
+    return (
+        <div>
+            <h1>Welcome to Prosperify!</h1>
+            <p>Prosperify is an amazing app for managing personal finances.</p>
+        </div>
+    );
+}
