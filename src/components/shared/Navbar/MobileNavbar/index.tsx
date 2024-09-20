3d493f639b634e0e3bb2a679286ce3147ad193a0
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2024-09-20 02:46:40 -0300

    Detalhe na navegação mobile

--- src/components/shared/Navbar/MobileNavbar/index.tsx
+++ src/components/shared/Navbar/MobileNavbar/index.tsx
@@ -6,14 +6,26 @@ import { Link } from "react-router-dom";
 const CustomBottomNavigation = styled(BottomNavigation)(({ theme }) => ({
     backgroundColor: theme.palette.background.paper,
     justifyContent: "space-around",
-    padding: "0.4rem",
-    height: "4rem",
+    padding: "0.6rem",
+    height: "4.75rem",
 }))
 
 const CustomBottomNavigationAction = styled(BottomNavigationAction)(({ theme }) => ({
     borderRadius: 8,
     maxWidth: 60,
     minWidth: 60,
+    transition: "background-color 0.25s",
+    "&::before": {
+        content: '""',
+        position: "absolute",
+        left: "50%",
+        right: "50%",
+        bottom: 4,
+        height: 4,
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: 40,
+        transition: "all 0.25s"
+    },
     "&.Mui-selected": {
         color: theme.palette.primary.main,
         backgroundColor: theme.palette.primary.contrastText,
