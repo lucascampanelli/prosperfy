ca4e01b2ec0a844b97c9d224e2ef9e1918e4c5bb
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2024-09-28 04:51:19 -0300

    Adicionando interações do card de contas

--- src/components/Accounts/AccountCard/index.tsx
+++ src/components/Accounts/AccountCard/index.tsx
@@ -1,10 +1,13 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+import { Card, CardActionArea, CardContent, IconButton, Tooltip, Typography, useTheme } from "@mui/material";
 import { IAccountCardProps } from "@typesrc/components/Accounts/AccountCard";
 import moment from "moment";
 
 export default function AccountCard({
     account
 }: IAccountCardProps) {
+
+    const theme = useTheme();
 
     return (
         <Card
@@ -14,29 +17,44 @@ export default function AccountCard({
                 color: theme => theme.palette.getContrastText(account.color ?? 'background.default'),
             }}
         >
-            <CardContent
-                className="flex flex-col h-full bg-transparent"
-            >
-                <div className="h-full">
-                    <Typography className="text-xl font-medium">
-                        {account.name}
+            <CardActionArea className="h-full">
+                <CardContent className="relative flex flex-col h-full bg-transparent">
+                    <Tooltip
+                        arrow
+                        title="Mais opções"
+                        placement="left"
+                    >
+                        <IconButton
+                            className="absolute top-4 right-4"
+                            size="large"
+                        >
+                            <MoreVert
+                                fontSize="medium"
+                                htmlColor={theme.palette.getContrastText(account.color ?? "#000000")}
+                            />
+                        </IconButton>
+                    </Tooltip>
+                    <div className="h-full">
+                        <Typography className="text-xl font-medium">
+                            {account.name}
+                        </Typography>
+                        <Typography className="text-3xl font-semibold">
+                            {
+                                Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(account.balance)
+                            }
+                        </Typography>
+                    </div>
+                    <Typography className="text-xs">
+                        Atualizado em:
                     </Typography>
-                    <Typography className="text-3xl font-semibold">
-                        {
-                            Intl.NumberFormat('pt-BR', {
-                                style: 'currency',
-                                currency: 'BRL'
-                            }).format(account.balance)
-                        }
+                    <Typography className="text-md">
+                        {moment(account.updatedAt).format("DD/MM/YYYY [às] HH:mm")}
                     </Typography>
-                </div>
-                <Typography className="text-xs">
-                    Atualizado em:
-                </Typography>
-                <Typography className="text-md">
-                    {moment(account.updatedAt).format("DD/MM/YYYY [às] HH:mm")}
-                </Typography>
-            </CardContent>
+                </CardContent>
+            </CardActionArea>
         </Card>
     )
 }
